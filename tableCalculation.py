b772478ed0725d7fdf86b87c02ad4c8d91d7a09d
Samuel-Giftson <sp1641@msstate.edu>
2021-07-31 23:07:10 -0500

    Added some parts to the calculation of values, still neeeds work

--- tableCalculation.py
+++ tableCalculation.py
@@ -11,9 +11,10 @@ class TableCalculation:
         self.main_pivot = self.pivot + 2
         self.side_pivot = self.pivot + 1
 
-        #Indexs it shouldn't pivot around
+        # Indexs it shouldn't pivot around
 
         self.main_list, self.no_pivot_index = self.form_table()
+        print(self.no_pivot_index)
         self.main_list = self.populate_table_gap_penalty_value()
 
         print(self.main_list)
@@ -28,10 +29,10 @@ class TableCalculation:
         no_pivot_index = [x_, y_]
         loop_amount = len(self.second_strand) + 1
         for i in range(loop_amount):
-            #print(i, loop_amount)
+            # print(i, loop_amount)
             x_ = x_ + loop_variant
             y_ = x_ + 1
-            no_pivot_index.append(x_)
+            # no_pivot_index.append(x_)
             no_pivot_index.append(y_)
 
         del no_pivot_index[-1]
@@ -70,18 +71,18 @@ class TableCalculation:
             self.main_list[main_pivot + i] = temp_gap_penalty + gap_penalty
             temp_gap_penalty = temp_gap_penalty + gap_penalty
 
-        side_pivot1 = main_pivot +side_pivot
+        side_pivot1 = main_pivot + side_pivot
         temp_gap_penalty = 0
         print("pivot main_pivot side_pivot side_pivot1")
-        if(len(self.first_strand)<len(self.second_strand)):
+        if (len(self.first_strand) < len(self.second_strand)):
             for j in range(side_pivot - 1):
                 print(pivot, main_pivot, side_pivot, side_pivot1)
                 self.main_list[side_pivot1] = temp_gap_penalty + gap_penalty
-                #print(side_pivot1, temp_gap_penalty, gap_penalty)
+                # print(side_pivot1, temp_gap_penalty, gap_penalty)
                 side_pivot1 = side_pivot1 + side_pivot
                 temp_gap_penalty = temp_gap_penalty + gap_penalty
             return self.main_list
-        elif(len(self.first_strand)>len(self.second_strand)):
+        elif (len(self.first_strand) > len(self.second_strand)):
             for j in range(side_pivot - 3):
                 print(pivot, main_pivot, side_pivot, side_pivot1)
                 self.main_list[side_pivot1] = temp_gap_penalty + gap_penalty
@@ -91,4 +92,41 @@ class TableCalculation:
             return self.main_list
 
     def calculate_table_values(self):
-        pass
+        loop_main_pivot = self.main_pivot
+        value_calculated = 0
+        f_strand = list(self.first_strand)
+        s_strand = list(self.second_strand)
+        f_strand.insert(0, "*")
+        f_strand.insert(0, "S")
+        s_strand.insert(0, "*")
+        s_strand.insert(0, "S")
+
+
+        f_strand_index = 0
+        s_strand_index = 0
+
+
+
+        #        f_strand_index = 0
+        #        s_strand_index = 0
+        no_index_index_value = self.no_pivot_index[2]
+
+        for i in range(len(self.main_list)):
+            if(loop_main_pivot<=(len(self.main_list)-1)):
+                if(loop_main_pivot>no_index_index_value):
+
+                #               if(loop_main_pivot > no_index_index_value):
+                #                   s_strand_index = s_strand_index + 1
+                #                   f_strand_index = 0
+                if self.main_list[loop_main_pivot] == " ":
+                    # ________________
+                    # | box1  | box2 |
+                    # |-------|------|
+                    # | box3  | box4 |
+                    # ---------------|
+
+                    box1_value = self.main_list[loop_main_pivot - self.main_pivot]
+                    box2_value = self.main_list[loop_main_pivot - (self.main_pivot - 1)]
+                    box3_value = self.main_list[loop_main_pivot - 1]
+
+                    self.main_list[loop_main_pivot] = value_calculated
